1d69d4baf1966e85e3349bbb3a83a5869a44838f
XMMM <turarbek191100@gmail.com>
2023-12-21 06:12:25 +0600

    Добавление бургер меню на страницу sign-in.php

--- sign-in.php
+++ sign-in.php
@@ -27,6 +27,8 @@
     <link rel="stylesheet" href="css/sign-in.css">
     <link rel="stylesheet" href="css/message.css">
     <link rel="website icon" type="png" href="images/logo_2.png">
+
+    <script defer src="js/adaptive_menu.js"></script>
 </head> 
 <body>
     <div class="wrap"> 
